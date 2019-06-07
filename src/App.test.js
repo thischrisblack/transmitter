aeab0900551ad911d58a64c0cae962f9107261cd
Chris Black <black@chrisblack.net>
2019-06-06 20:59:58 -0700

    Update App.test.js
    Works with Firebase Auth now.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./components/App";
+import Firebase, { FirebaseContext } from "./components/Firebase";
 
-xit("renders without crashing", () => {
+it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
